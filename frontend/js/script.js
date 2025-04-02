313d1d72f6967563e2c68030535417495da5b896
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-04-02 16:52:24 -0300

    Styling with css and starting kanban and dark and light mode with JavaScript

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -17,13 +17,47 @@ buttonToggle.addEventListener("click", () => {
 
 const buttonToggle = document.querySelector("#light-mode-toggle");
 
+// Verifica se o usuário já tem um tema salvo
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light-mode");
+  buttonToggle.innerHTML = '<i class="fa-solid fa-toggle-on"></i>';
+}
+
 function toggleDarkMode() {
   document.body.classList.toggle("light-mode");
 
-  const buttonToggle = document.querySelector("#light-mode-toggle");
+  // Atualiza o botão
   buttonToggle.innerHTML = document.body.classList.contains("light-mode")
     ? '<i class="fa-solid fa-toggle-on"></i>'
     : '<i class="fa-solid fa-toggle-off"></i>';
-}
 
+  // Salva a preferência no localstorage
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("light-mode") ? "light" : "dark"
+  );
+}
 buttonToggle.addEventListener("click", toggleDarkMode);
+
+const tasks = document.querySelectorAll(".task");
+const containers = document.querySelectorAll(".card");
+
+tasks.forEach((task) => {
+  task.addEventListener("dragstart", () => {
+    task.classList.add("dragging");
+  });
+
+  task.addEventListener("dragend", () => {
+    task.classList.remove("dragging");
+  });
+});
+
+containers.forEach((card) => {
+  card.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    const draggingTask = document.querySelector(".dragging");
+    card.appendChild(draggingTask);
+  });
+});
+
+
